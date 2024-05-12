9453fe38a175fda4aeee5c0512ad37075ac9e498
Lenetis <70149832+Lenetis@users.noreply.github.com>
2024-05-12 17:22:58 +0200

    Cool effects

--- AR Education/Assets/Code/Scripts/BezierCurves/BezierDrawer.cs
+++ AR Education/Assets/Code/Scripts/BezierCurves/BezierDrawer.cs
@@ -8,11 +8,6 @@ public class BezierDrawer : MonoBehaviour
 {
     public Transform[] Points;
 
-    [Range(2, 200)]
-    public int Resolution;
-
-    public Vector3 Offset;
-
     private LineRenderer lineRenderer;
     private Vector3[] previousPositions;
 
@@ -20,20 +15,36 @@ public class BezierDrawer : MonoBehaviour
     void Start()
     {
         lineRenderer = GetComponent<LineRenderer>();
-        previousPositions = Enumerable.Repeat(Vector3.zero, Points.Length).ToArray();
+        Refresh();
     }
 
     // Update is called once per frame
     void Update()
     {
-        var positions = Points.Select(Point => Point.position + Offset).ToArray();
+        var positions = Points.Select(Point => Point.position + BezierManager.Instance.Offset).ToArray();
         
         if (!positions.SequenceEqual(previousPositions)) {
-            var linePoints = BezierUtility.GeneratePoints(positions, Resolution);
-            lineRenderer.positionCount = Resolution;
+            var linePoints = BezierUtility.GeneratePoints(positions, BezierManager.Instance.Resolution,
+                                                          BezierManager.Instance.StartOvershoot, BezierManager.Instance.EndOvershoot, BezierManager.Instance.Overshoot);
+            lineRenderer.positionCount = BezierManager.Instance.Resolution;
             lineRenderer.SetPositions(linePoints);
         }
 
         previousPositions = positions;
+    }
+
+    private void OnDrawGizmos()
+    {
+        var positions = Points.Select(Point => Point.position).ToArray();
+        var linePoints = BezierUtility.GeneratePoints(positions, 200);  // default resolution of 200, as the DrawManager.Instance doesn't exist in editor
+
+        for (int i = 0; i < linePoints.Length - 1; i += 1) {
+            Gizmos.DrawLine(linePoints[i], linePoints[i + 1]);
+        }
+    }
+
+    public void Refresh()
+    {
+        previousPositions = Enumerable.Repeat(Vector3.zero, Points.Length).ToArray();        
     }
 }
--- AR Education/Assets/Code/Scripts/BezierCurves/BezierUtility.cs
+++ AR Education/Assets/Code/Scripts/BezierCurves/BezierUtility.cs
@@ -12,21 +12,26 @@ public class BezierUtility
         return BezierInternal(pointsCopy, t, points.Length);
     }
 
-    public static Vector3[] GeneratePoints(Vector3[] points, int resolution) {
+    public static Vector3[] GeneratePoints(Vector3[] points, int resolution, float startOvershoot = 0f, float endOvershoot = 0f, float overshoot = 0f) {
         Vector3[] newPoints = new Vector3[resolution];
         for (int i = 0; i < resolution; i += 1) {
-            newPoints[i] = Bezier(points, (float)i / (resolution - 1));
+            float t = (float)i / (resolution - 1);
+            newPoints[i] = Bezier(points, t * (endOvershoot + 1f + startOvershoot) - startOvershoot + overshoot);
         }
         return newPoints;
     }
 
     private static Vector3 BezierInternal(Vector3[] points, float t, int length) {
         if (length == 2){
-            return Vector3.Lerp(points[0], points[1], t);
+            return OvershootLerp(points[0], points[1], t);
         }
         for (int i = 0; i < length - 1; i += 1){
-            points[i] = Vector3.Lerp(points[i], points[i+1], t);
+            points[i] = OvershootLerp(points[i], points[i+1], t);
         }
         return BezierInternal(points, t, length - 1);
+    }
+
+    private static Vector3 OvershootLerp(Vector3 a, Vector3 b, float t) {
+        return a + (b - a) * t;
     }
 }
