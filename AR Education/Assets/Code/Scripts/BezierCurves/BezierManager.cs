cb1fbb9e0b6625a27dbe8e28d4e14883507bc6ba
Lenetis <70149832+Lenetis@users.noreply.github.com>
2024-05-12 22:05:28 +0200

    Naive points checking

--- AR Education/Assets/Code/Scripts/BezierCurves/BezierManager.cs
+++ AR Education/Assets/Code/Scripts/BezierCurves/BezierManager.cs
@@ -2,31 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using System.Linq;
+using System;
 
 public class BezierManager : MonoBehaviour
 {
     public static BezierManager Instance;
 
-    private BezierDrawer[] drawers;
-
-    
-
     [Range(2, 200)]
     public int Resolution;
-
     [Range(-4, 4)]
     public float Overshoot;
-
     [Range(-1, 1)]
     public float StartOvershoot;
-
     [Range(-1, 1)]
     public float EndOvershoot;
-
     public Vector3 Offset;
 
-    //public BezierPattern[] Patterns;
-    public float ErrorThreshold = 0.1f;
+    [SerializeField] private float ErrorThreshold = 0.5f;
+
+    [SerializeField] private GameObject[] Patterns;
+
+    private Vector3[] points;  // reference points of the pattern
+    private List<Tracker> trackers = new List<Tracker>();  // trackers checked against points
 
     
 
@@ -46,19 +44,52 @@ public class BezierManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //foreach (BezierPattern pattern in Patterns) {
+        var pattern = Patterns[0];  // todo
+        
+        points = pattern.GetComponentsInChildren<BezierTracker>().Select(tracker => tracker.gameObject.transform.position).ToArray();
+        for (int i = 0; i < points.Length; i += 1) {
+            points[i].y = 0f;
+        }
 
-        //}
+        var allTrackers = GameObject.FindObjectsOfType<BezierTracker>();
+        if (allTrackers.Length < points.Length) {
+            throw new Exception("Not enough trackers detected");
+        }
+
+        for (int i = 0; i < points.Length; i += 1) {
+            trackers.Add(allTrackers[i]);
+        }
+
+        Instantiate(pattern);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (CheckCompletion()) {
+            Debug.Log("HOORAYY!");
+        }
+                
     }
 
     private bool CheckCompletion()
     {
+        if (trackers.Count == 0) {
+            return false;
+        }
+
+        foreach (var tracker in trackers) {
+            bool ok = false;
+            foreach (var point in points) {
+                if (Vector3.SqrMagnitude(Vector3.Scale(tracker.gameObject.transform.position, new Vector3(1f, 0f, 1f)) - point) < ErrorThreshold) {
+                    ok = true;
+                    break;
+                }
+            }
+            if (!ok) {
+                return false;
+            }
+        }
         return true;
     }
 
